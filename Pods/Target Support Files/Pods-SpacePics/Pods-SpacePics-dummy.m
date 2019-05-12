3193c81ca5a1a60da69e5e905d64490caab9a50c
Rajeev <rajeevcbhatia@gmail.com>
2019-05-12 20:45:27 +0200

    added pods working on startVC

---
+++ Pods/Target Support Files/Pods-SpacePics/Pods-SpacePics-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SpacePics : NSObject
+@end
+@implementation PodsDummy_Pods_SpacePics
+@end
---
+++ SpacePics/Start/StartViewController.swift
@@ -0,0 +1,9 @@
+//
+//  StartViewController.swift
+//  SpacePics
+//
+//  Created by Rajeev Bhatia on 12/05/19.
+//  Copyright © 2019 rajeev. All rights reserved.
+//
+
+import Foundation
