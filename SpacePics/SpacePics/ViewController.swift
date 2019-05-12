8def3b1b934297460f15c952a9c67370c6c38ff5
Rajeev <rajeevcbhatia@gmail.com>
2019-05-12 19:55:30 +0200

    initial commit

---
+++ SpacePics/SpacePics/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SpacePics
+//
+//  Created by Rajeev Bhatia on 12/05/19.
+//  Copyright © 2019 rajeev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
